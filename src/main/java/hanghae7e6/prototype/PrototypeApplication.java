b8dd95a8e2597c6aa53d3c0d738127d9f865deb3
Blueberry <54464320+lee01097@users.noreply.github.com>
2022-06-25 16:54:07 +0900

    init project

---
+++ src/main/java/hanghae7e6/prototype/PrototypeApplication.java
@@ -0,0 +1,12 @@
+package hanghae7e6.prototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrototypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrototypeApplication.class, args);
+	}
+}
